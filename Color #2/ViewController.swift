6f89817760b5c7f1ab3d63c94e72cce3a46bfe0d
KENNEDY <JOHNSAMUELKENNEDY1@GMAIL.COM>
2021-10-16 22:32:46 -0400

    Initial Commit

---
+++ Color #2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Color #2
+//
+//  Created by John Kennedy on 10/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
